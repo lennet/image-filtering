204310151e876e7b4155799240ae1f87e0c11ce1
Leonard Thomas <leo.thomas@me.com>
2017-04-24 19:49:04 +0200

    added a link to the successmessage

--- Image Filtering.playgroundbook/Contents/Sources/SourcesIpadOnly/ConvolutionDemoViewControllerExtension.swift
+++ Image Filtering.playgroundbook/Contents/Sources/SourcesIpadOnly/ConvolutionDemoViewControllerExtension.swift
@@ -4,7 +4,7 @@ import PlaygroundSupport
 extension ConvolutionDemoViewController {
 
     public func showSuccessMessage() {
-        PlaygroundLiveViewSessionManager.shared.sendPassMessage(message: "Yeah! You successfully convolved the image with a filter kernel. Proceed to the next pages to learn about different filters and how they work")
+        PlaygroundLiveViewSessionManager.shared.sendPassMessage(message: "Yeah! You successfully convolved the image with a filter kernel. Proceed to the [next pages](@next) to learn about different filters and how they work")
     }
 
 }
