a45601cda61cfd9b4a02af6e0f00fbc899eedb98
Leonard Thomas <leo.thomas@me.com>
2017-04-24 19:46:59 +0200

    moved PanDirection into separate file

--- Image Filtering.playgroundbook/Contents/Sources/ConvolutionDemo/ConvolutionDemoViewController.swift
+++ Image Filtering.playgroundbook/Contents/Sources/ConvolutionDemo/ConvolutionDemoViewController.swift
@@ -587,52 +587,6 @@ public class ConvolutionDemoViewController: UIViewController, UIGestureRecognize
     
     // mark - UIGestureRecognizer
     
-    enum PanDirection {
-        case left
-        case topLeft
-        case top
-        case topRight
-        case right
-        case bottomRight
-        case bottom
-        case bottomLeft
-        case none
-        
-        init(translation: CGPoint) {
-            let minDistance: CGFloat = 10
-            
-            switch (translation.x, translation.y) {
-            case let (x,y) where x > minDistance && y > minDistance:
-                self = .topRight
-                break
-            case let (x,y) where x < -minDistance && y < -minDistance:
-                self = .bottomLeft
-                break
-            case let (x,y) where x < -minDistance && y > minDistance:
-                self = .topLeft
-                break
-            case let (x,y) where x > minDistance && y < -minDistance:
-                self = .bottomRight
-                break
-            case let (x,_) where x > minDistance:
-                self = .right
-                break
-            case let (x,_) where x < -minDistance:
-                self = .left
-                break
-            case let (_,y) where y > minDistance:
-                self = .top
-                break
-            case let (_,y) where y < -minDistance:
-                self = .bottom
-                break
-            default:
-                self = .none
-                break
-            }
-        }
-    }
-    
     func handlePan(sender: UIPanGestureRecognizer, state: UIGestureRecognizerState) {
         // a custom state parameter is needed to get all state changes
         
@@ -688,7 +642,6 @@ public class ConvolutionDemoViewController: UIViewController, UIGestureRecognize
                 break
             }
             sender.setTranslation(.zero, in: view)
-            
             
             currentPoint.x = min(max(0, currentPoint.x), CGFloat(contentWidth-1))
             currentPoint.y = min(max(0, currentPoint.y), CGFloat(contentWidth-1))
---
+++ Image Filtering.playgroundbook/Contents/Sources/Helper/PanDirection.swift
@@ -0,0 +1,47 @@
+import CoreGraphics
+
+public enum PanDirection {
+    case left
+    case topLeft
+    case top
+    case topRight
+    case right
+    case bottomRight
+    case bottom
+    case bottomLeft
+    case none
+    
+    public init(translation: CGPoint) {
+        let minDistance: CGFloat = 10
+        
+        switch (translation.x, translation.y) {
+        case let (x,y) where x > minDistance && y > minDistance:
+            self = .topRight
+            break
+        case let (x,y) where x < -minDistance && y < -minDistance:
+            self = .bottomLeft
+            break
+        case let (x,y) where x < -minDistance && y > minDistance:
+            self = .topLeft
+            break
+        case let (x,y) where x > minDistance && y < -minDistance:
+            self = .bottomRight
+            break
+        case let (x,_) where x > minDistance:
+            self = .right
+            break
+        case let (x,_) where x < -minDistance:
+            self = .left
+            break
+        case let (_,y) where y > minDistance:
+            self = .top
+            break
+        case let (_,y) where y < -minDistance:
+            self = .bottom
+            break
+        default:
+            self = .none
+            break
+        }
+    }
+}
