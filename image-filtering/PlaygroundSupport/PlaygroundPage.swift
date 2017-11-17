60971609b8c020bb378549bf8169e65034b0ce22
Leonard Thomas <leo.thomas@me.com>
2017-11-17 19:19:16 +0100

    added xcode project for future development

---
+++ image-filtering/PlaygroundSupport/PlaygroundPage.swift
@@ -0,0 +1,66 @@
+//
+//  PlaygroundPage.swift
+//  PlaygroundSupport
+//
+//  Created by Leo Thomas on 13.11.17.
+//  Copyright © 2017 Leonard Thomas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol LiveViewable {}
+
+extension UIView: LiveViewable {}
+extension UIViewController: LiveViewable {}
+
+public enum AssesmentStatus {
+    case fail(hints:[String],solution: String?)
+    case pass(message: String)
+}
+
+public class PlaygroundPage {
+    public static var current = PlaygroundPage()
+    public var assessmentStatus: AssesmentStatus?
+    public var liveView: LiveViewable!
+    public var needsIndefiniteExecution = true
+    public func finishExecution() {}
+}
+
+public protocol PlaygroundRemoteLiveViewProxyDelegate {
+    
+}
+
+public class PlaygroundRemoteLiveViewProxy {
+    public var delegate: Any?
+}
+
+public enum PlaygroundValue {
+    case array([PlaygroundValue])
+    case integer(Int)
+    case string(String)
+    case data(Data)
+    case dictionary([String: PlaygroundValue])
+    case boolean(Bool)
+    case floatingPoint(Float)
+}
+
+public class PlaygroundKeyValueStore {
+    public static var current = PlaygroundKeyValueStore()
+    public subscript(parameter: String) -> PlaygroundValue? {
+        get {
+            return nil
+        }
+        set {
+            
+        }
+    }
+}
+
+public protocol PlaygroundLiveViewMessageHandler {
+    func send(_ message: PlaygroundValue)
+}
+
+extension PlaygroundLiveViewMessageHandler {
+    public func send(_ message: PlaygroundValue) {}
+}
---
+++ image-filtering/PlaygroundSupport/PlaygroundSupport.h
@@ -0,0 +1,19 @@
+//
+//  PlaygroundSupport.h
+//  PlaygroundSupport
+//
+//  Created by Leo Thomas on 17.11.17.
+//  Copyright © 2017 Leonard Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaygroundSupport.
+FOUNDATION_EXPORT double PlaygroundSupportVersionNumber;
+
+//! Project version string for PlaygroundSupport.
+FOUNDATION_EXPORT const unsigned char PlaygroundSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundSupport/PublicHeader.h>
+
+
